7b7e9440576e357a7d5b7127d05516f8bd836d15
Tim Fitzgerald <diceytroop@gmail.com>
2021-07-09 11:06:06 -0400

    Record button now determines whether ChatClient is joined or parted from a channel, addressing part of #25

--- src/hooks/useChannel.tsx
+++ src/hooks/useChannel.tsx
@@ -7,24 +7,25 @@ const useChannel = (channelName: string) => {
   const { chatClient, loggedIn } = useChatClient()
 
   const channel = useAppSelector(state => state.channels[channelName]);
+  const isScanning = channel.scanning
   const [joined, setJoined] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
 
   // join / part channel
   useEffect(() => {
     // console.log('join part channel: ', chatClient, joined, error, loggedIn)
-    if (chatClient && !joined && !error && loggedIn) {
+    if (chatClient && !joined && !error && loggedIn && isScanning) {
       chatClient.join(channelName).then((success) => setJoined(true), (err) => setError(err));
       setJoined(true)
     }
 
     return (() => {
-      if (chatClient && joined && !error && (typeof channel === 'undefined')) {
+      if (chatClient && joined && !error && !(typeof channel === 'undefined')) {
         chatClient.part(channelName)
         setJoined(false)
       }
     })
-  }, [joined, error, chatClient, channel, loggedIn, channelName])
+  }, [joined, error, chatClient, channel, loggedIn, channelName, isScanning])
 
   return channel
 
